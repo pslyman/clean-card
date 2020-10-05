5d3ac47c2c194d0d832640caeef74e12be83f789
Porter Lyman <porter.lyman@healthcatalyst.com>
2020-10-05 16:59:06 -0600

    Adding in Angular routes, since it's a lil nicer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  /* If you want to change the route name, change it here AND in home.component.ts (nameOfRoute string) */
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
